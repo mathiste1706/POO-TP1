bc3ff18e5a7562ef3b43cc6ea635a4f0d760411b
Lalanne Mathilde <mathilde1706.lalanne@gmail.com>
2024-01-19 09:17:40 +0100

    Fin

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -16,6 +16,11 @@ public String getNom() {
 public void parler(String texte) {
 	System.out.println("Le gaulois "+nom+": "+"\""+texte+"\".");
 }
+
+public int frapper(Romain romain) {
+	System.out.print(nom+" envoie un grand coup dans la mâchoire de "+romain.getNom());
+	return (force/3);
+}
 public static void main(String[] args) {
 	Gaulois asterix = new Gaulois("Asterix", 8);
 	System.out.println(asterix.getNom());
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,13 +1,26 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 
 	public static void main(String[] args) {
+		int forceCoup;
 		Gaulois asterix = new Gaulois("Asterix", 8);
+		Romain minus=new Romain("Minus", 6);
+		
 		System.out.println(asterix.getNom());
 		asterix.parler("Bonjour à tous");
+		minus.parler("UN GAU... UN GAUGAU...");
+		/*for (int i=0; i<2;i++) {
+			minus.recevoirCoup(3);
+		}*/
+		for (int i=0; i<3;i++) {
+		forceCoup=asterix.frapper(minus);
+		minus.recevoirCoup(forceCoup);
+	}
+		
 
 	}
 }
