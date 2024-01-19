61e2f2de021a96eed694049e3f79608275deb145
Lalanne Mathilde <mathilde1706.lalanne@gmail.com>
2024-01-19 08:52:38 +0100

    Ajout methode parler

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,25 @@
 package personnages;
 
 public class Gaulois {
+private String nom;
+private int force;
 
+public Gaulois(String nom, int force){
+	this.nom=nom;
+	this.force=force;
 }
+
+public String getNom() {
+	return nom;
+}
+
+public void parler(String texte) {
+	System.out.println("Le gaulois "+nom+": "+"\""+texte+"\".");
+}
+public static void main(String[] args) {
+	Gaulois asterix = new Gaulois("Asterix", 8);
+	System.out.println(asterix.getNom());
+	
+}
+}
+
---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,13 @@
+package test_fonctionnel;
+
+import personnages.Gaulois;
+
+public class TestGaulois {
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		System.out.println(asterix.getNom());
+		asterix.parler("Bonjour à tous");
+
+	}
+}
